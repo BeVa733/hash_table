ed21a9ce2bb21a7400422fd689f113cf8a842ed9
Belousov Vadim <vadim.belousov.2000@inbox.ru>
2026-04-12 23:32:32 +0300

    third optimize and profiling

--- read_modul/onegin.cpp
+++ read_modul/onegin.cpp
@@ -4,68 +4,91 @@
 #include <assert.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdint.h>
+#include <immintrin.h>
 
 #include "hash_table.h"
 
-static inline bool is_sep(char symbol)
-{
-    return symbol == ' ' || symbol == '\n' || symbol == '\t';
-}
-
-static char** make_ptr_massive(char* buffer, long* num_lines, size_t read_size)
+char** make_ptr_massive(char* buffer, long* num_lines, size_t read_size)
 {
     assert(buffer != NULL);
     assert(num_lines != NULL);
 
-    long capacity = read_size >> 3;
-    char** lines = (char**)calloc(capacity, sizeof(char*));
-    if (!lines)
+    long capacity = (read_size >> 1) + 1;
+    char** words = (char**)calloc(capacity, sizeof(char*));
+    if (!words)
     {
         free(buffer);
         return NULL;
     }
 
-    long line_index = 0;
+    long word_index = 0;
     bool inside_word = false;
 
-    for (size_t i = 0; i < read_size; i++)
+    __m256i spaces = _mm256_set1_epi8(' ');
+    size_t i = 0;
+
+    for (; i + 32 <= read_size; i += 32)
     {
-        if (is_sep(buffer[i]))
+        char* ptr = buffer + i;
+        uint32_t sep_mask = 0;
+
+        asm volatile (
+            ".intel_syntax noprefix                 \n\t"
+
+            "vmovdqu      ymm0, YMMWORD PTR [%[p]]  \n\t"
+            "vpcmpeqb     ymm1, ymm0, %[sp]         \n\t"
+            "vpmovmskb    %[mask], ymm1             \n\t"
+
+            "vpandn       ymm2, ymm1, ymm0          \n\t"   // repace  ' ' to \0
+            "vmovdqu      YMMWORD PTR [%[p]], ymm2  \n\t"
+
+            ".att_syntax prefix                     \n\t"
+            : [mask] "=r"(sep_mask)
+            : [p]  "r"(ptr),
+              [sp] "x"(spaces)
+            : "ymm0", "ymm1", "ymm2", "ymm5", "memory"
+        );
+
+        uint32_t first_bit  = inside_word ? 0 : 1;
+        uint32_t start_mask = (~sep_mask) & ((sep_mask << 1) | first_bit);
+
+        while (start_mask)
+        {
+            unsigned bit_index = (unsigned)__builtin_ctz(start_mask);
+            words[word_index++] = ptr + bit_index;
+            start_mask &= start_mask - 1;
+        }
+
+        inside_word = (((sep_mask >> 31) & 1) == 0);
+    }
+
+    asm volatile ("vzeroupper" ::: "memory");
+
+    for (; i < read_size; i++)
+    {
+        if (buffer[i] == ' ')
         {
             buffer[i] = '\0';
             inside_word = false;
         }
         else if (!inside_word)
         {
-            if (line_index >= capacity)
-            {
-                capacity *= 2;
-                char** new_lines = (char**)realloc(lines, capacity * sizeof(char*));
-                if (!new_lines)
-                {
-                    free(lines);
-                    free(buffer);
-                    return NULL;
-                }
-
-                lines = new_lines;
-            }
-
-            lines[line_index++] = &buffer[i];
+            words[word_index++] = &buffer[i];
             inside_word = true;
         }
     }
 
-    *num_lines = line_index;
+    *num_lines = word_index;
 
     if (*num_lines == 0)
     {
-        free(lines);
+        free(words);
         free(buffer);
         return NULL;
     }
 
-    return lines;
+    return words;
 }
 
 static long int check_file_size(FILE* file)
@@ -74,7 +97,7 @@ static long int check_file_size(FILE* file)
     int fd = fileno(file);
     if (fstat(fd, &file_info)== -1)
     {
-        printf("ERROR: check number of lines incorrect\n");
+        printf("ERROR: check number of words incorrect\n");
         return -1;
     }
 
